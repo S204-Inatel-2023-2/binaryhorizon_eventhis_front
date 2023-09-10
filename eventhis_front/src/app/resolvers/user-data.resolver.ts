389e16076d0c7acd64fe0678124f59a3a90f1d96
airtong <airton-carvalho@outlook.com>
2023-09-10 03:40:44 -0300

    [backend integration] profile showing user data

--- eventhis_front/src/app/resolvers/user-data.resolver.ts
+++ eventhis_front/src/app/resolvers/user-data.resolver.ts
@@ -8,7 +8,6 @@ export class UserDataResolver {
     constructor(private authService: AuthService) {}
 
     resolve() {
-        console.log('Hello');
         return this.authService.getUserData();
     }
 }
--- eventhis_front/src/app/tabs/profile/profile-routing.module.ts
+++ eventhis_front/src/app/tabs/profile/profile-routing.module.ts
@@ -1,11 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ProfilePage } from './profile.page';
+import { UserDataResolver } from '../../resolvers/user-data.resolver';
 
 const routes: Routes = [
   {
     path: '',
     component: ProfilePage,
+    resolve: {
+      userData: UserDataResolver
+    }
   }
 ];
 
