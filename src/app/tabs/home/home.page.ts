c688eca370aa22e27f36d9e8be285d00ff6b4988
airtong <airton-carvalho@outlook.com>
2023-10-11 13:24:16 -0300

    [feature] home page dinamically generated

--- src/app/tabs/home/home.page.ts
+++ src/app/tabs/home/home.page.ts
@@ -1,5 +1,9 @@
 import { Component } from '@angular/core';
 import { IonicSlides } from '@ionic/angular';
+import { Router } from '@angular/router';
+import { ActivatedRoute } from '@angular/router';
+import { ToastService } from '../../services/toast.service';
+import { AuthService } from 'src/app/services/auth.service';
 
 @Component({
   selector: 'app-home',
@@ -10,35 +14,113 @@ export class HomePage {
 
   swiperModules = [IonicSlides];
 
+  events: any
+  events1: any
+  events2: any
+  friends: any
+  user: any
+
   doRefresh(event: any) {
     setTimeout(() => {
       event.detail.complete();
     }, 2000);
   }
+
+
+  constructor(
+    private route: ActivatedRoute,
+    private authService: AuthService,
+    private toastService: ToastService,
+    private router: Router
+  ) {}
+
+  goToProfile(user_id: string) {
+    this.router.navigate(['/community', user_id]);
+  }
   
-  eventos = [
-    {
-      titulo: 'Festival',
-      data: new Date(),
-      imagem: 'https://i.pinimg.com/564x/31/e6/1d/31e61d2e45e57a8cdf15186c25654327.jpg'
-    },
-    {
-      titulo: 'Hacktown 2023',
-      data: new Date(),
-      imagem: 'https://i.pinimg.com/564x/4c/95/53/4c95537739d93bea4dee245b8ed28fba.jpg'
-    },
-    {
-      titulo: 'Festival 2',
-      data: new Date(),
-      imagem: 'https://i.pinimg.com/564x/31/e6/1d/31e61d2e45e57a8cdf15186c25654327.jpg'
-    },
-    {
-      titulo: 'Hacktown 2053',
-      data: new Date(),
-      imagem: 'https://i.pinimg.com/564x/4c/95/53/4c95537739d93bea4dee245b8ed28fba.jpg'
+  goToEventDetails(eventId: string) {
+    this.router.navigate(['/events/' + eventId]);
+  }
+
+  ngOnInit() {
+    this.authService.getAllEvents().subscribe({
+      next: (res: any) => {
+        if (res['events']) {
+          const eventsData = res['events'];
+
+          if(eventsData) {
+            this.events = eventsData;
+
+            for (let i = 0; i < this.events.length; i++) {
+              if(this.events[i].photo === "") {
+                this.events[i].photo = "https://placehold.co/600x400";
+              }
+
+              if(this.events[i].name.length > 20) {
+                this.events[i].name = this.events[i].name.substring(0, 20) + "...";
+              }
+            }
+          }
+
+          this.events1 = this.events.slice(0, Math.ceil(this.events.length / 2));
+          this.events2 = this.events.slice(Math.ceil(this.events.length / 2), this.events.length);
+          
+        } else {
+          this.toastService.presentToast('Could not find event.');
+
+          this.router.navigate(['/events']);
+        }
+      },
+      error: (error: any) => {
+        this.toastService.presentToast("Could not find event.");
+
+        this.router.navigate(['/events']);
+      }
+    });
+
+    try {
+      let userData = this.route.snapshot.data['userData'];
+      if (userData) {
+        this.user = userData;
+      } else {
+        this.router.navigate(['/login']);
+      }
+    } catch (error) {
+      console.error('Erro ao obter dados do usuário:', error);
     }
-  ]
 
-  constructor() {}
+    this.authService.getAllUsers().subscribe({
+      next: (res: any) => {
+        if (res['users']) {
+          let usersData = res['users'];
 
+          if(usersData) {
+            this.friends = usersData;
+
+            for (let i = 0; i < this.friends.length; i++) {
+              this.friends[i].contact.name = this.friends[i].contact.name.split(' ')[0];
+              
+              console.log(this.friends[i])
+
+              if(this.friends[i].user_id === this.user.user_id) {
+                this.friends.splice(i, 1);
+                i--; // Decrement i to account for the removed element
+              }
+
+              if(this.friends[i].contact.photo === "") {
+                this.friends[i].contact.photo = "https://placehold.co/100x100";
+              }
+            }
+
+            console.log(this.friends)
+          }
+        } else {
+          this.toastService.presentToast('Could not find friends.');
+        }
+      },
+      error: (error: any) => {
+        this.toastService.presentToast("Could not find friends.");
+      }
+    });
+  }
 }
