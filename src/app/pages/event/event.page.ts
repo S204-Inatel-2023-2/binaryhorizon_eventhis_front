5e8e6fe9baffe2ad44f03dbbe7d98fade85b8381
airtong <airton-carvalho@outlook.com>
2023-10-11 17:57:53 -0300

    [feature] event screen custom for owner
    Co-authored-by: lrgsouza <lucas_souza@outlook.com>

--- src/app/pages/event/event.page.ts
+++ src/app/pages/event/event.page.ts
@@ -11,6 +11,9 @@ import { ToastService } from 'src/app/services/toast.service';
 export class EventPage implements OnInit {
   eventId!: string
   event: any
+  user: any
+  is_owner: boolean = false
+  is_subscribed: boolean = false
 
   constructor(
     private route: ActivatedRoute,
@@ -18,20 +21,78 @@ export class EventPage implements OnInit {
     private toastService: ToastService,
     private router: Router
   ) {}
-  doSomething() {
-    console.log('Something');
+
+  editEvent() {
+    console.log("Volte mais tarde!")
   }
-  ngOnInit() {
+
+  unsubscribeFromEvent() {
+    console.log("Volte mais tarde!")
+  }
+
+  async subscribeToEvent() {
+
+    if (this.user) {
+
+      this.authService.subscribeToEvent(this.eventId, this.user.user_id).subscribe({
+        next: (res: any) => {
+          if (res['success']) {
+            this.toastService.presentToast('Você se inscreveu nesse evento.');
+          } else {
+            this.toastService.presentToast('Não foi possível se inscrever nesse evento.');
+          }
+        },
+        error: (error: any) => {
+          this.toastService.presentToast("Could not find event.");
+  
+          this.router.navigate(['/events']);
+        }
+      });
+     
+    } else {
+      this.router.navigate(['/login']);
+      this.toastService.presentToast('Você precisa estar logado para se inscrever em um evento!');
+    }
+  }
+
+  ngOnInit() {}
+
+  ionViewWillEnter() {
     this.route.params.subscribe(params => {
       this.eventId = params['id'];
       
       this.authService.getEventData(this.eventId).subscribe({
-        next: (res: any) => {
+        next: async (res: any) => {
           if (res['event']) {
             const eventData = res['event'];
 
             if(eventData) {
               this.event = eventData;
+
+              try {
+                const userData = await this.route.snapshot.data['userData'];
+                if (userData) {
+                  this.user = userData;
+
+                  if(this.event.host.user_id == this.user.user_id) {
+                    this.is_owner = true;
+                  }
+
+                  let participants = this.event.participants;
+                  for (let i = 0; i < participants.length; i++) {
+                    if (participants[i].user_id == this.user.user_id) {
+                      this.is_subscribed = true;
+                      break;
+                    }
+                  }
+
+                } else {
+                  this.user = null;
+                }
+              } catch (error) {
+                console.error('Usuário não está logado!');
+              }
+
 
               if(this.event.photo === "") {
                 this.event.photo = "https://placehold.co/600x400";
