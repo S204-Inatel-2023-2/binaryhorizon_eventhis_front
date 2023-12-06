ff0cff6b03c6894f82ed4e376aca3db721f0f69e
airtong <airton-carvalho@outlook.com>
2023-12-06 00:10:34 -0300

    [feature] community showing only your friends

--- src/app/tabs/community/community.page.ts
+++ src/app/tabs/community/community.page.ts
@@ -46,22 +46,20 @@ export class CommunityPage {
       console.error('Erro ao obter dados do usuário:', error);
     }
 
-    this.authService.getAllUsers().subscribe({
+    this.authService.getConnections(this.user['user_id']).subscribe({
       next: (res: any) => {
-        if (res['users']) {
-          let usersData = res['users'];
+        if (res['success']) {
+          let connections = res['connections'];
 
-          if(usersData) {
-            this.friends = usersData;
-            for (let i = 0; i < this.friends.length; i++) {
-              this.friends[i].contact.name = this.CapitalizeFirstLetter(this.friends[i].contact.name);
-
-              if(this.friends[i].contact.photo === "") {
-                this.friends[i].contact.photo = "https://placehold.co/100x100";
-              }
-              if(this.friends[i].user_id === this.user.user_id) {
-                this.friends.splice(i, 1);
-                i--; // Decrement i to account for the removed element
+          for (let i = 0; i < connections.length; i++) {
+            if(connections) {
+              this.friends = connections;
+              for (let i = 0; i < this.friends.length; i++) {
+                this.friends[i].contact.name = this.CapitalizeFirstLetter(this.friends[i].contact.name);
+  
+                if(this.friends[i].contact.photo === "") {
+                  this.friends[i].contact.photo = "https://placehold.co/100x100";
+                }
               }
             }
           }
